5ee2a15db0cf43a1e50d98522701917c3ec21dde
Bolaji Ayodeji <sirbeejay1@gmail.com>
2024-01-23 18:00:36 +0100

    chore: update endpoints

--- spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -16,5 +16,14 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+		// test url: http://localhost:8080/hello?name=Bolaji
+	}
+
+	@GetMapping("/add")
+	public int add(
+			@RequestParam(value = "x", defaultValue = "0") int x,
+			@RequestParam(value = "y", defaultValue = "0") int y) {
+		return x + y;
+		// test url: http://localhost:8080/add?x=12&y=76
 	}
 }
